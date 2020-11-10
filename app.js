4c280cd71a858e3f4c9ef9b7740c4816a1f4e976
AnggaChelsea <muhidabdul168@gmail.com>
2020-11-10 23:27:57 +0700

    coba tambah Procfile dan tambah process.env

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const morgan = require('morgan');
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 mongooseConnect();
 app.use(morgan('dev'));
