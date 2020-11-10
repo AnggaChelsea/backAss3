80cb5754b62ceea996323e9ae78ef031a3320202
Angga Lesmana <muhidabdul168@gmail.com>
2020-11-10 17:10:39 +0700

    error get

--- routes/categoryfake.js
+++ routes/categoryfake.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const CategoryControllerfake = require('../controllers/CategoryControllerfake')
 
-router.get('/',CategoryControllerfake.get);
+router.get('/categoty',CategoryControllerfake.get);
 
 //display all produts in a specific Category
 router.get('/:category',);
--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const userController = require('../controllers/UserController');
 
 router.post('/register', userController.register);
 router.post('/login', userController.login);
-router.get('/', userController.getList);
+router.get('/data', userController.getList);
 router.get('/:id', userController.getId)
 router.put('/:id', userController.editUser);
 router.delete('/:id', userController.delete);
